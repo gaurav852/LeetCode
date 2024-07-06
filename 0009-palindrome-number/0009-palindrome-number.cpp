0a2f1d981fc67d596509d2d39e65e5b675fcef3c
Gaurav Singh <58967582+gaurav852@users.noreply.github.com>
2024-07-06 21:57:42 +0530

    Time: 12 ms (33.2%), Space: 8.2 MB (47.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false; // Negative numbers cannot be palindromes
+        }
+
+        long long reversed = 0;
+        int original = x;
+
+        while (x > 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return reversed == original;
+    }
+};
